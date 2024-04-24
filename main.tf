b3f055ebe9e29cb2c255ae89ee71f687dd36026f
Tim DB <timdeblock@combits.be>
2024-04-24 19:15:47 +0200

    replace storage account

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
-  name                     = "stgcombitstftest002"
+  name                     = "stgcombitstftest005"
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
